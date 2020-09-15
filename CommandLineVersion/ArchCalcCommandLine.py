a67f59c9a93af2af707ade4f09e82d271d11d467
Jeanie Chea <Jeanie@Jeanies-MacBook-Pro.local>
2020-09-15 00:55:16 -0700

    Show information of each material, Add methods

--- CommandLineVersion/ArchCalcCommandLine.py
+++ CommandLineVersion/ArchCalcCommandLine.py
@@ -11,15 +11,44 @@ custom_agent = {
     'From': 'youremail@domain.com'
 }
 
-artefact = input("Artefact: ")
-amt = int(input("Amount: "))
+artifact = input("Artifact: ")
+
+url_start = "https://runescape.wiki/w/"
+full_url = url_start + artifact
+status_check = requests.get(full_url, headers=custom_agent)
+
+# Check if artifact input is valid
+if status_check.status_code == 200:
+    url = requests.get(full_url, headers=custom_agent).text
+else:
+    raise Exception('Invalid artifact. Please try again.')
+    
+# Check for invalid number
+while True:
+    try:
+        amt = int(input("Amount: "))
+        break
+    except ValueError:
+        print("Invalid number.  Please try again.")
+
+# while True:
+#     try:
+#         url = requests.get(full_url, headers=custom_agent, timeout=5)
+#         break
+#     except requests.exceptions.HTTPError as errh:
+#         print ("Http Error:",errh)
+#         exit()
+#     except requests.exceptions.ConnectionError as errc:
+#         print ("Error Connecting:",errc)
+#         exit()
+#     except requests.exceptions.Timeout as errt:
+#         print ("Timeout Error:",errt)
+#         exit()
+#     except requests.exceptions.RequestException as err:
+#         print ("OOps: Something Else",err)
+#         exit()
 
 # Web scrape the RS Wiki to find the material data
-url_start = "https://runescape.wiki/w/"
-full_url = url_start + artefact
-
-url = requests.get(full_url, headers=custom_agent).text
-
 soup = BeautifulSoup(url, 'lxml')
 
 My_table = soup.find_all('table', class_='wikitable')[1]
@@ -57,26 +86,68 @@ while index2 < len(total_price):
 # Create class to store material data
 class Materials:
     def __init__(self, mat_name, mat_amount, mat_price, total_mat_price):
-        self.mat_name = mat_name
-        self.mat_amount = int(mat_amount)
-        self.mat_price = int(mat_price)
-        self.total_mat_price = int(total_mat_price)
+        self._mat_name = mat_name
+        self._mat_amount = int(mat_amount)
+        self._mat_price = int(mat_price)
+        self._total_mat_price = int(total_mat_price)
+        
+    @property
+    def MatName(self):
+        return self._mat_name
+    
+    @property
+    def MatAmount(self):
+        return self._mat_amount
+    
+    @property
+    def MatPrice(self):
+        return self._mat_price
+
+    @property
+    def TotalMatPrice(self):
+        return self._total_mat_price
+    
+    @MatAmount.setter
+    def MatAmount(self, mat_amount):
+        self._mat_amount = mat_amount
+
+    @TotalMatPrice.setter
+    def TotalMatPrice(self, total_mat_price):
+        self._total_mat_price = total_mat_price
 
 # Store each Material class object into a list
 list2 = []
 
 for i in range(len(mat_name)):
-    mat1 = list2.append(Materials(mat_name[i], mat_amount[i], mat_price[i], total_mat_price[i]))
+    list2.append(Materials(mat_name[i], 
+                           mat_amount[i], 
+                           mat_price[i], 
+                           total_mat_price[i]))
 
 # Calculate the total price of all materials
-def mult(amount):
+def totalCost(amount):
     s = 0
     for item in list2:
-        s += item.total_mat_price
+        s += item.TotalMatPrice
     s *= amount
-    return s
+    return s    
 
-sum = mult(amt)
+# Multiply the material amount and total material amount 
+# by the amount of artifacts
+def calcMats(amount):
+    for i in range(len(mat_name)):
+        list2[i].MatAmount *= amount
+        list2[i].TotalMatPrice *= amount 
 
-# Print out summary
-print(f"Total cost of {amt} {artefact}(s) is: {sum} gp")
+sum = totalCost(amt)
+calcMats(amt)
+
+# Print summary
+for item in list2:
+    print(f'''
+Material Name: {item.MatName}
+Amount: {item.MatAmount}
+Price: {item.MatPrice} gp
+Total Price: {item.TotalMatPrice} gp\n''')
+    
+print(f"\nTotal cost of {amt} {artifact}(s) is: {sum} gp\n")
