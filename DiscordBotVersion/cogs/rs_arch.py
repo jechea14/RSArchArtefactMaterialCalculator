bbe2f9a0ee05cf460041e1cf54de80bd27c692ba
Jeanie Chea <chea.jeanie@gmail.com>
2024-05-29 04:26:49 -0700

    resolved #2

--- DiscordBotVersion/cogs/rs_arch.py
+++ DiscordBotVersion/cogs/rs_arch.py
@@ -5,78 +5,80 @@ from bs4 import BeautifulSoup
 from discord.ext import commands
 from material import Materials
 from experience import ArtifactExp
-            
+
+
 class RsArchCalc(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    
+
     @commands.command()
     async def calc(self, ctx):
-        
+
         def check(self, ctx):
             return lambda m: m.author == ctx.author and m.channel == ctx.channel
 
         async def get_input_of_type(self, func, ctx):
             while True:
                 try:
-                    msg = await self.bot.wait_for('message', 
-                                                  check = check(self, ctx))
+                    msg = await self.bot.wait_for("message", check=check(self, ctx))
                     return func(msg.content)
                 except ValueError:
                     continue
-        
+
         # Webscrape the RS wiki for artifact materials
         def webscrape_materials(self, table):
             temp_list = []
 
-            for item in table.find_all('tbody'):
-                for a in item.find_all('tr')[9:]:
-                    for b in a.find_all('td'):
+            for item in table.find_all("tbody"):
+                for a in item.find_all("tr")[9:]:
+                    for b in a.find_all("td"):
                         temp = b.get_text()
                         temp_list.append(temp)
-            return temp_list 
-        
+            return temp_list[1:-5]
+
         # Webscrape the RS wiki for artifact experience
         def webscrape_experience(self, table):
-            experience_data = table.tbody.find_all('tr')[1]
-            experience_text = experience_data.find('td').get_text()
-            experience = experience_text.split()
-            del(experience[1])
-            return experience
-        
+            experience_data = table.tbody.find_all("tr")[6]
+            experience_text = experience_data.find_all("td")[-1].get_text()
+            return experience_text.replace(",", "")
+
         # Remove commas for future calculations
         def remove_comma(self, list):
             index = 0
             while index < len(list):
-                list[index] = list[index].replace(',', '')
-                index += 1    
+                list[index] = list[index].replace(",", "")
+                index += 1
             return list
-        
+
         # Store each Material class object into a list
         def store(self, list):
             obj_mats = []
+
             index = 0
-            while index < len(list)-1:
-                obj_mats.append(Materials(list[index+1], 
-                                    list[index+2], 
-                                    list[index+3], 
-                                    list[index+4]))
-                index += 5
+            while index < len(list) - 1:
+                obj_mats.append(
+                    Materials(
+                        list[index],
+                        list[index + 1],
+                        list[index + 2].replace(",", ""),
+                    )
+                )
+                index += 4
             return obj_mats
-        
+
         # Return total cost of material prices
         def total_cost(self, list):
             sum_price = 0
             for item in list:
                 sum_price += item.total_mat_price
-            return sum_price 
+            return sum_price
 
-        # Multiply the material amount and total material amount 
+        # Multiply the material amount and total material amount
         # by the amount of artifacts
         def calc_mats(self, amount, list):
             for i in range(len(list)):
                 list[i].mat_amount *= amount
-                list[i].total_mat_price *= amount 
+                list[i].total_mat_price = amount * list[i].mat_price
 
         # Total experience without full archaeology outfit
         def exp_without_outfit(self, amount, experience):
@@ -89,7 +91,7 @@ class RsArchCalc(commands.Cog):
             outfit = 0.06
             exp = amount * (experience.experience + (experience.experience * outfit))
             return exp
-        
+
         # Get inputs from user
         await ctx.send("What Artifact do you want to restore?")
         artifact = await get_input_of_type(self, str, ctx)
@@ -99,8 +101,8 @@ class RsArchCalc(commands.Cog):
         # Please provide a custom user agent to send information about yourself to be
         # considerate to the admins
         custom_agent = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36',
-        'From': 'your-email@domain.com'
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36",
+            "From": "your-email@domain.com",
         }
 
         # Show the bot is typing
@@ -115,71 +117,74 @@ class RsArchCalc(commands.Cog):
             if status_check.status_code == 200:
                 url = requests.get(full_url, headers=custom_agent).text
             else:
-                await ctx.send('Invalid artifact. Please try again.')
-                raise Exception('Invalid artifact. Please try again.')
+                await ctx.send("Invalid artifact. Please try again.")
+                raise Exception("Invalid artifact. Please try again.")
 
             # Web scrape the RS Wiki to find the material data
-            soup = BeautifulSoup(url, 'lxml')
+            soup = BeautifulSoup(url, "lxml")
 
-            wiki_mat_table = soup.find_all('table', class_='wikitable')[1]
+            wiki_mat_table = soup.find_all("table", class_="wikitable")[1]
 
             # Get artifact image
-            img = wiki_mat_table.find('a').find('img').get('src')
-            start = 'https://runescape.wiki'
-            img_url = start + img 
-            
-            material_list = webscrape_materials(self, wiki_mat_table)
-            experience_info = webscrape_experience(self, wiki_mat_table)            
+            img = wiki_mat_table.find("a").find("img").get("src")
+            start = "https://runescape.wiki"
+            img_url = start + img
 
-            remove_comma(self, material_list)
-            temp_exp = remove_comma(self, experience_info)
-            
-            experience = float(temp_exp[0])
-            artifact_exp = ArtifactExp(experience) 
-            
+            material_list = webscrape_materials(self, wiki_mat_table)
+            experience_info = webscrape_experience(self, wiki_mat_table)
+
+            experience = float(experience_info)
+            artifact_exp = ArtifactExp(experience)
+
             mat_list = store(self, material_list)
 
             calc_mats(self, amt, mat_list)
             capitalize = artifact.title()
-        
+
         # Embed message to make results look pretty
-        embed = discord.Embed(title='{:,} {}(s)'.format(amt, capitalize),
-                            color=discord.Color.blue()) 
-        
+        embed = discord.Embed(
+            title="{:,} {}(s)".format(amt, capitalize), color=discord.Color.blue()
+        )
+
         for item in mat_list:
-            embed.add_field(name=item.mat_name, 
-                            value='''Amount: {:,}
-                                    Price: {:,} gp
-                                    Total Price: {:,} gp'''.format(item._mat_amount,
-                                                                item._mat_price, 
-                                                                item._total_mat_price))
-        
-        embed.add_field(name="Total cost of materials:", 
-                        value='{:,} gp'.format(total_cost(self, 
-                                                          mat_list)), 
-                        inline=False)
-        
-        embed.add_field(name="XP with outfit:", 
-                    value='{:,} xp'.format(exp_with_outfit(self, 
-                                                           amt, 
-                                                           artifact_exp)), 
-                    inline=True)
-        
-        embed.add_field(name="XP without outfit:", 
-                    value='{:,} xp'.format(exp_without_outfit(self, 
-                                                              amt, 
-                                                              artifact_exp)), 
-                    inline=True)
-        
-        embed.add_field(name="XP each:", 
-                    value='{:,} xp'.format(artifact_exp.experience), 
-                    inline=True)
-        
+            embed.add_field(
+                name=item.mat_name,
+                value="""Amount: {:,}
+                                    Total Price: {:,} gp""".format(
+                    item._mat_amount, item._total_mat_price
+                ),
+            )
+
+        embed.add_field(
+            name="Total cost of materials:",
+            value="{:,} gp".format(total_cost(self, mat_list)),
+            inline=False,
+        )
+
+        embed.add_field(
+            name="XP with outfit:",
+            value="{:,} xp".format(exp_with_outfit(self, amt, artifact_exp)),
+            inline=True,
+        )
+
+        embed.add_field(
+            name="XP without outfit:",
+            value="{:,} xp".format(exp_without_outfit(self, amt, artifact_exp)),
+            inline=True,
+        )
+
+        embed.add_field(
+            name="XP each:",
+            value="{:,} xp".format(artifact_exp.experience),
+            inline=True,
+        )
+
         # User name
         embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
-        
+
         embed.set_thumbnail(url=img_url)
         await ctx.send(embed=embed)
-        
+
+
 def setup(bot):
     bot.add_cog(RsArchCalc(bot))
--- ScriptVersion/ArchCalcScript.py
+++ ScriptVersion/ArchCalcScript.py
@@ -4,6 +4,7 @@ from tabulate import tabulate
 from material import Materials
 from experience import ArtifactExp
 
+
 def main():
     artifact = input("Artifact: ")
 
@@ -14,41 +15,41 @@ def main():
     amt = check_amount()
 
     # Web scrape the RS Wiki
-    soup = BeautifulSoup(url, 'lxml')
-    wiki_mat_table = soup.find_all('table', class_='wikitable')[1]
+    soup = BeautifulSoup(url, "lxml")
+    wiki_mat_table = soup.find_all("table", class_="wikitable")[1]
 
     material_list = webscrape_materials(wiki_mat_table)
     experience_info = webscrape_experience(wiki_mat_table)
-    
-    remove_comma(material_list)
-    temp_exp = remove_comma(experience_info)
-    experience = float(temp_exp[0])
-    
+
+    experience = float(experience_info)
+
     mat_list = store(material_list)
-    
+
     calc_mats(amt, mat_list)
     artifact_exp = ArtifactExp(experience)
-    
+
     summary(mat_list, amt, artifact_exp)
+
 
 # Check if artifact input is valid
 def check_url(full_url):
     # Please provide a custom user agent to send information about yourself to
     # be considerate to the RS Wiki admins
     custom_agent = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36',
-        'From': 'your_email@domain.com'
-    }   
-    
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36",
+        "From": "your_email@domain.com",
+    }
+
     status_check = requests.get(full_url, headers=custom_agent)
 
     if status_check.status_code == 200:
         url = requests.get(full_url, headers=custom_agent).text
         return url
     else:
-        raise Exception('Invalid artifact. Please try again.')
+        raise Exception("Invalid artifact. Please try again.")
 
-# Check for invalid number    
+
+# Check for invalid number
 def check_amount():
     while True:
         try:
@@ -58,47 +59,53 @@ def check_amount():
         except ValueError:
             print("Invalid number.  Please try again.")
 
+
 # Webscrape the RS wiki for artifact materials
 def webscrape_materials(table):
     temp_list = []
 
-    for item in table.find_all('tbody'):
-        for a in item.find_all('tr')[9:]:
-            for b in a.find_all('td'):
+    for item in table.find_all("tbody"):
+        for a in item.find_all("tr")[9:]:
+            for b in a.find_all("td"):
                 temp = b.get_text()
                 temp_list.append(temp)
-    
-    return temp_list
+
+    return temp_list[1:-5]
+
 
 # Webscrape the RS wiki for artifact experience
 def webscrape_experience(table):
-    experience_data = table.tbody.find_all('tr')[1]
-    experience_text = experience_data.find('td').get_text()
-    experience = experience_text.split()
-    del(experience[1])
-    return experience
+    experience_data = table.tbody.find_all("tr")[6]
+    experience_text = experience_data.find_all("td")[-1].get_text()
+    return experience_text.replace(",", "")
+
 
 # Remove commas for future calculations
 def remove_comma(list):
     index = 0
     while index < len(list):
-        list[index] = list[index].replace(',', '')
+        list[index] = list[index].replace(",", "")
         index += 1
-        
+
     return list
+
 
 # Store each Material class object into a list
 def store(list):
     obj_mats = []
 
     index = 0
-    while index < len(list)-1:
-        obj_mats.append(Materials(list[index+1], 
-                               list[index+2], 
-                               list[index+3], 
-                               list[index+4]))
-        index += 5
+    while index < len(list) - 1:
+        obj_mats.append(
+            Materials(
+                list[index],
+                list[index + 1],
+                list[index + 2].replace(",", ""),
+            )
+        )
+        index += 4
     return obj_mats
+
 
 # Return total cost of material prices
 def total_cost(list):
@@ -107,17 +114,20 @@ def total_cost(list):
         sum_price += item.total_mat_price
     return sum_price
 
-# Multiply the material amount and total material amount 
+
+# Multiply the material amount and total material amount
 # by the amount of artifacts
 def calc_mats(amount, list):
     for i in range(len(list)):
         list[i].mat_amount *= amount
-        list[i].total_mat_price *= amount 
+        list[i].total_mat_price = amount * list[i].mat_price
+
 
 # Total experience without full archaeology outfit
 def exp_without_outfit(amount, experience):
     exp = experience.experience * amount
     return exp
+
 
 # Total experience with full archaeology outfit
 def exp_with_outfit(amount, experience):
@@ -126,12 +136,12 @@ def exp_with_outfit(amount, experience):
     exp = amount * (experience.experience + (experience.experience * outfit))
     return exp
 
+
 # Formatting for summary. Too many lists and for-loops
 def summary(mat_list, amt, experience):
-    headers = ['Material', 'Amount', 'Price', 'Total Price']
+    headers = ["Material", "Amount", "Total Price"]
     name = []
     amount = []
-    price = []
     total_price = []
 
     for item in mat_list:
@@ -141,23 +151,20 @@ def summary(mat_list, amt, experience):
         amount.append("{:,}".format(item.mat_amount))
 
     for item in mat_list:
-        price.append("{:,} gp".format(item.mat_price))
-
-    for item in mat_list:
         total_price.append("{:,} gp".format(item.total_mat_price))
-        
-    table = zip(name, amount, price, total_price)
-    print(f'\n{tabulate(table, headers=headers)}')
 
-    xpheader = ['Total Cost', 
-                'XP with outfit', 
-                'XP without outfit',
-                'XP each']
-    table1 = zip(["{:,} gp".format(total_cost(mat_list))], 
-                 ["{:,} xp".format(exp_with_outfit(amt, experience))], 
-                 ["{:,} xp".format(exp_without_outfit(amt, experience))],
-                 ["{:,} xp".format(experience.experience)])
-    print(f'\n{tabulate(table1, headers=xpheader)}')  
+    table = zip(name, amount, total_price)
+    print(f"\n{tabulate(table, headers=headers)}")
+
+    xpheader = ["Total Cost", "XP with outfit", "XP without outfit", "XP each"]
+    table1 = zip(
+        ["{:,} gp".format(total_cost(mat_list))],
+        ["{:,} xp".format(exp_with_outfit(amt, experience))],
+        ["{:,} xp".format(exp_without_outfit(amt, experience))],
+        ["{:,} xp".format(experience.experience)],
+    )
+    print(f"\n{tabulate(table1, headers=xpheader)}")
+
 
 if __name__ == "__main__":
     main()
